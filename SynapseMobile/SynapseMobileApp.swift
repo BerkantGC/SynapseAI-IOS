cf489cdce5f5bd8ce80afca50f984bbd81f1151b
Berkant Gürcan <berkantgurcan@hotmail.com>
2024-11-10 12:08:15 +0300

    Initial Commit

---
+++ SynapseMobile/SynapseMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynapseMobileApp.swift
+//  SynapseMobile
+//
+//  Created by Berkant Gürcan on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SynapseMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
