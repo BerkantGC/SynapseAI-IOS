1210412e3f5b39188e18b760acb3d814e24d3725
Berkant Gürcan <berkantgurcan@hotmail.com>
2024-11-16 14:24:34 +0300

    move: story components

---
+++ SynapseMobile/Components/Home/Story/StoryModalView.swift
@@ -0,0 +1,67 @@
+//
+//  StoryModalView.swift
+//  SynapseMobile
+//
+//  Created by Berkant Gürcan on 16.11.2024.
+//
+
+import SwiftUI
+
+struct StoryModalView: View {
+    @State private var offset: CGSize = .zero
+    @State private var isDragging = false
+    let story: StoryModel
+    @Binding var showStoryModal: Bool
+    var animationNamespace: Namespace.ID
+
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.9).ignoresSafeArea()
+            .onTapGesture {
+                withAnimation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.5)) {
+                    showStoryModal = false
+                }
+            }
+            
+            VStack {
+                Spacer()
+                AsyncImage(url: URL(string: "http://localhost:8080/image/\(story.image).png")) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .matchedGeometryEffect(id: story.id, in: animationNamespace)
+                        .frame(width: .infinity, height: .infinity)
+                }placeholder: {
+                    ProgressView()
+                }
+                
+                Text(story.user)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+
+                Spacer()
+            }.offset(offset)
+                .scaleEffect( 1 - offset.height/UIScreen.main.bounds.height )
+                .gesture(DragGesture().onChanged(onDrag).onEnded(onEnd))
+        }.toolbarVisibility(.hidden, for: .tabBar)
+        .zIndex(1)
+    }
+    
+    func onDrag(_ value: DragGesture.Value) -> Void {
+        withAnimation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.5)) {
+            offset = value.translation
+            isDragging = true
+        }
+    }
+    
+    func onEnd(_ value: DragGesture.Value) -> Void {
+        withAnimation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.5)) {
+            if offset.height > 200 {
+                showStoryModal = false
+            }
+            offset = .zero
+            isDragging = false
+        }
+    }
+}
--- SynapseMobile/View/Tabs/HomeView.swift
+++ SynapseMobile/View/Tabs/HomeView.swift
@@ -57,65 +57,6 @@ struct HomeView: View {
 }
 
 
-struct StoryModalView: View {
-    @State private var offset: CGSize = .zero
-    @State private var isDragging = false
-    let story: StoryModel
-    @Binding var showStoryModal: Bool
-    var animationNamespace: Namespace.ID
-
-    var body: some View {
-        ZStack {
-            Color.black.opacity(0.9).ignoresSafeArea()
-            .onTapGesture {
-                withAnimation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.5)) {
-                    showStoryModal = false
-                }
-            }
-            
-            VStack {
-                Spacer()
-                AsyncImage(url: URL(string: "http://localhost:8080/image/\(story.image).png")) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .matchedGeometryEffect(id: story.id, in: animationNamespace)
-                        .frame(width: .infinity, height: .infinity)
-                }placeholder: {
-                    ProgressView()
-                }
-                
-                Text(story.user)
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-
-                Spacer()
-            }.offset(offset)
-                .scaleEffect( 1 - offset.height/UIScreen.main.bounds.height )
-                .gesture(DragGesture().onChanged(onDrag).onEnded(onEnd))
-        }.toolbarVisibility(.hidden, for: .tabBar)
-        .zIndex(1)
-    }
-    
-    func onDrag(_ value: DragGesture.Value) -> Void {
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.5)) {
-            offset = value.translation
-            isDragging = true
-        }
-    }
-    
-    func onEnd(_ value: DragGesture.Value) -> Void {
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.5)) {
-            if offset.height > 200 {
-                showStoryModal = false
-            }
-            offset = .zero
-            isDragging = false
-        }
-    }
-}
-
 #Preview {
     Main()
 }
